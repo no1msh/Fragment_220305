b989b695eb3a91b2dda3f18c6829f658b66ad3c8
TFQ_Moon <no1msh1217@gmail.com>
2022-03-05 12:48:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fragment_220305/fragments/FirstFragment.kt
@@ -0,0 +1,37 @@
+package com.example.fragment_220305.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragment_220305.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_first , container, false)
+    }
+
+//    프래그먼트에서의 동작 관련 코드
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLogInFragment.setOnClickListener {
+
+            Log.d("프래그먼트", "첫번째 프래그먼트에서 로그 찍기")
+            
+        }
+    }
+
+
+
+}
